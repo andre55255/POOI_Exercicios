46d1ffa089b75eb8554e7d1bbd06d270f06a9f7f
andre55255 <andre_luiz.b5@outlook.com>
2021-09-07 21:07:16 -0300

    Arrumando a questão 5

--- ListaDeExercicio1/Ex5/src/ex5/Ex5.java
+++ ListaDeExercicio1/Ex5/src/ex5/Ex5.java
@@ -4,12 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Ex5 extends JApplet{
+    
+    public double Hipotenusa(double a, double b) {
+        return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    }
+    
     @Override
     public void paint(Graphics g) {
         double a = Double.parseDouble(JOptionPane.showInputDialog(this, "Cálculo da hipotenusa\nDigite o 1° cateto: "));
         double b = Double.parseDouble(JOptionPane.showInputDialog(this, "Cálculo da hipotenusa\nDigite o 2° cateto: "));
         
-        double h = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        double h = Hipotenusa(a, b);
         
         JOptionPane.showMessageDialog(this, String.format("Dados os catetos digitados:\n"
                 + "%.1f e %.1f,\n"
